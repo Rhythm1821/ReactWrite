55f025c4f6b441cd12f85cf192652b4bc38d2c60
Rhythm1821 <alamrawat1821@gmail.com>
2024-05-27 18:34:45 +0530

    Reading and adding post backend functionality

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.PostList.as_view(),name='posts'),
+    path('',views.PostListView.as_view(),name='posts'),
+    path('edit/',views.PostUpdateView.as_view(),name='edit_post'),
 ]
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
-from .models import Post
+from .models import Post,Profile
 from .serializers import PostSerializer
 
 # Create your views here.
-class PostList(generics.ListCreateAPIView):
-    # queryset = Post.objects.all()
+class PostListView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
     
     def get_queryset(self):
         posts = Post.objects.all()
-        author_username = self.request.query_params.get('author', None)
-        print("Author", author_username)
-        if author_username is not None:
+        author_username = self.request.query_params.get('author', None or '')
+        if author_username is not None and author_username != '':
             posts = posts.filter(author__user__username=author_username)
         return posts
+    
+    def perform_create(self, serializer):
+        user = self.request.user
+        profile = Profile.objects.get(user=user)
+        serializer.save(author=profile)
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        post=Post.objects.get(pk=self.kwargs['pk'])
+        return post
+
+    def perform_update(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+            print(f'Post updated for user: {self.request.user}')
