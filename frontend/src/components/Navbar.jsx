5efc74996ceb6555fe51d2069bc7b810c35d50f1
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-23 17:12:21 +0530

    updates for production

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -136,7 +136,7 @@ function Navbar(props) {
                   onClick={handleClick}
                 >
                   <img
-                    src={user?.image || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+                    src={user?.image?.trim() ? user.image : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
                     height={50}
                     width={50}
                     alt=""
