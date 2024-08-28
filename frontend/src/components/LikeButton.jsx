1f66074c07af5daa5b9edda326ab0d4e31db6002
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-28 23:00:29 +0530

    Adding like btn

--- frontend/src/components/LikeButton.jsx
+++ frontend/src/components/LikeButton.jsx
@@ -3,14 +3,11 @@ import React, { useEffect, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { styled, useTheme } from '@mui/system';
 import api from '../api';
 
 const LikeButton = ({ postId, numOfLikes }) => {
   const [toggleNumLikes, setToggleNumLikes] = useState(numOfLikes);
   const [liked, setLiked] = useState(false);
-  const theme = useTheme();
-
   const handleLikeClick = async () => {
     setLiked(!liked);
     try {
@@ -34,16 +31,13 @@ const LikeButton = ({ postId, numOfLikes }) => {
     fetchUserLikeStatus();
   }, [postId]);
 
-  const StyledIconButton = styled(IconButton)({
-    color: liked ? theme.palette.secondary.main : theme.palette.grey[500],
-  });
-
   return (
-    <>
-    <StyledIconButton onClick={handleLikeClick}>
-      {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />} {toggleNumLikes}
-    </StyledIconButton>
-    </>
+    <div className='flex items-center text-gray-700'>
+    <IconButton onClick={handleLikeClick} aria-label="like">
+      {liked ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
+    </IconButton>
+    {toggleNumLikes}
+    </div>
   );
 };
 
--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -29,7 +29,7 @@ export default function PostDetail() {
     const theme = createTheme();
 
     const post = initialPost || {};
-
+    
     const handleDelete = async (id) => {
         try {
             await api.delete(`/posts/delete/${id}/`);
@@ -69,7 +69,7 @@ export default function PostDetail() {
                     <CardContent>
                         <Box onClick={() => navigate(`/profile/${post.author.id}/`)} sx={{ display: 'flex', alignItems: 'center', mb: 3, cursor: 'pointer' }}>
                             <Avatar
-                                src={user?.image ? `${import.meta.env.VITE_IMAGE_BASE_URL}${user.image}` : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+                                src={post?.author.image ? `${import.meta.env.VITE_IMAGE_BASE_URL}${post.author.image}` : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
                                 alt={"https://cdn-icons-png.flaticon.com/512/149/149071.png"}
                                 sx={{ width: 56, height: 56, mr: 2 }}
                             />
