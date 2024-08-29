7a8a68a43adf91080d5834c7f93f64e151c3e58e
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-29 14:55:07 +0530

    Like btn speed increased

--- frontend/src/components/LikeButton.jsx
+++ frontend/src/components/LikeButton.jsx
@@ -1,5 +1,4 @@
-// LikeButton.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -8,36 +7,40 @@ import api from '../api';
 const LikeButton = ({ postId, numOfLikes }) => {
   const [toggleNumLikes, setToggleNumLikes] = useState(numOfLikes);
   const [liked, setLiked] = useState(false);
-  const handleLikeClick = async () => {
-    setLiked((prevLiked) => !prevLiked);
-    setToggleNumLikes((prevNum)=> liked ? prevNum - 1 : prevNum + 1);
-    try {
-        const response = await api.post(`/posts/toggle-like-button/${postId}/`);
-        setToggleNumLikes(response.data);
-    } catch (error) {
-        console.error('Failed to toggle like', error);
-    }
-  };
 
-  const fetchUserLikeStatus = async () => {
+  const handleLikeClick = useCallback(async () => {
+    setLiked((prevLiked) => !prevLiked);
+    setToggleNumLikes((prevNum) => liked ? prevNum - 1 : prevNum + 1);
+
     try {
-      const response = await api.get(`/posts/toggle-like-button/${postId}/`);
-      setLiked(response.data)
+      await api.post(`/posts/toggle-like-button/${postId}/`);
     } catch (error) {
-      console.error('Failed to fetch user in post likes', error);
+      // Revert the UI state if the request fails
+      setLiked((prevLiked) => !prevLiked);
+      setToggleNumLikes((prevNum) => liked ? prevNum + 1 : prevNum - 1);
+      console.error('Failed to toggle like', error);
     }
-  };
+  }, [liked, postId]);
 
   useEffect(() => {
+    const fetchUserLikeStatus = async () => {
+      try {
+        const response = await api.get(`/posts/toggle-like-button/${postId}/`);
+        setLiked(response.data);
+      } catch (error) {
+        console.error('Failed to fetch user in post likes', error);
+      }
+    };
+
     fetchUserLikeStatus();
   }, [postId]);
 
   return (
     <div className='flex items-center text-gray-700'>
-    <IconButton onClick={handleLikeClick} aria-label="like">
-      {liked ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
-    </IconButton>
-    {toggleNumLikes}
+      <IconButton onClick={handleLikeClick} aria-label="like">
+        {liked ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
+      </IconButton>
+      {toggleNumLikes}
     </div>
   );
 };
